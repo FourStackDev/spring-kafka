5d1f28b7c1f713343aefb9ae5c84b6ae3c1109f9
FourStackDev <fourstackdevelopers@gmail.com>
2022-10-19 12:41:16 +0530

    Added new kafka project

---
+++ library-inventory/library-event-producer/src/test/java/org/fourstack/kafka/libraryeventproducer/LibraryEventProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fourstack.kafka.libraryeventproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
