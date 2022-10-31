2172c6105e57793ed98668caa7dc6c287c608113
FourStackDev <fourstackdevelopers@gmail.com>
2022-10-31 16:28:41 +0530

    Added Exceptions to be ignored for Retry attempts

--- library-inventory/library-event-consumer/src/main/java/org/fourstack/kafka/libraryeventconsumer/config/KafkaConsumerConfig.java
+++ library-inventory/library-event-consumer/src/main/java/org/fourstack/kafka/libraryeventconsumer/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package org.fourstack.kafka.libraryeventconsumer.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -11,16 +12,45 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Configuration
+@Slf4j
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private List<String> bootstrapServers;
+
+    /**
+     * ErrorHandler to handle the errors of Kafka Consumer.
+     *
+     * @return
+     */
+    public DefaultErrorHandler errorHandler() {
+        //create a BackOff with 1 second interval and 5 attempts
+        var backOff = new FixedBackOff(1000L, 5);
+        var errorHandler = new DefaultErrorHandler(backOff);
+
+        var exceptionsIgnoreList = List.of(
+                IllegalArgumentException.class,
+                ArithmeticException.class
+        );
+        exceptionsIgnoreList.forEach(errorHandler :: addNotRetryableExceptions);
+        Recovera
+        // Set Retry Listeners to ErrorHandler.
+        errorHandler.setRetryListeners(
+                (record, ex, deliveryAttempt) -> {
+                    log.info("Failed record in Retry attempt, Exception - {}, deliveryAttempt - {}",
+                            ex.getMessage(), deliveryAttempt);
+                }
+        );
+        return errorHandler;
+    }
 
     public Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
@@ -38,9 +68,10 @@ public class KafkaConsumerConfig {
 
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>>
-    listenerFactory (ConsumerFactory<Integer, String> consumerFactory) {
+    listenerFactory(ConsumerFactory<Integer, String> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler());
         return factory;
     }
 }
