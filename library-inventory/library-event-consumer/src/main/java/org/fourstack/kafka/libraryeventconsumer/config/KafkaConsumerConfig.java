446970595611c0f7a683288b1297a3f36fd866e6
FourStackDev <fourstackdevelopers@gmail.com>
2022-11-01 17:38:20 +0530

    Added code for DeadLetterPublishingRecoverer - Recovery logic for failed Records

--- library-inventory/library-event-consumer/src/main/java/org/fourstack/kafka/libraryeventconsumer/config/KafkaConsumerConfig.java
+++ library-inventory/library-event-consumer/src/main/java/org/fourstack/kafka/libraryeventconsumer/config/KafkaConsumerConfig.java
@@ -2,16 +2,21 @@ package org.fourstack.kafka.libraryeventconsumer.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
@@ -26,6 +31,41 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private List<String> bootstrapServers;
 
+    @Value("${spring.kafka.topic.retry}")
+    private String retryTopic;
+
+    @Value("${spring.kafka.topic.dlt}")
+    private String deadLetterTopic;
+
+    /*
+     * KafkaTemplate Bean is created for the purpose of Recovery logic,
+     * where failed Records will be published to the new retry topics using the KafkaTemplate
+     */
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate() {
+        Map<String, Object> producerProps = new HashMap<>();
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        ProducerFactory<Integer, String> factory = new DefaultKafkaProducerFactory<>(producerProps);
+
+        KafkaTemplate<Integer, String> template = new KafkaTemplate<>(factory);
+        return template;
+    }
+
+    public DeadLetterPublishingRecoverer publishingRecoverer(KafkaTemplate<Integer, String> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate,
+                (consumerRecord, exception) -> {
+                    if (exception.getCause() instanceof RecoverableDataAccessException) {
+                        return new TopicPartition(retryTopic, consumerRecord.partition());
+                    } else {
+                        return new TopicPartition(deadLetterTopic, consumerRecord.partition());
+                    }
+                });
+        return recoverer;
+    }
+
     /**
      * ErrorHandler to handle the errors of Kafka Consumer.
      *
@@ -34,14 +74,14 @@ public class KafkaConsumerConfig {
     public DefaultErrorHandler errorHandler() {
         //create a BackOff with 1 second interval and 5 attempts
         var backOff = new FixedBackOff(1000L, 5);
-        var errorHandler = new DefaultErrorHandler(backOff);
+        var errorHandler = new DefaultErrorHandler(publishingRecoverer(kafkaTemplate()), backOff);
 
         var exceptionsIgnoreList = List.of(
                 IllegalArgumentException.class,
                 ArithmeticException.class
         );
-        exceptionsIgnoreList.forEach(errorHandler :: addNotRetryableExceptions);
-        Recovera
+        exceptionsIgnoreList.forEach(errorHandler::addNotRetryableExceptions);
+
         // Set Retry Listeners to ErrorHandler.
         errorHandler.setRetryListeners(
                 (record, ex, deliveryAttempt) -> {
